14b8680ee1bdf8cd5466ec955ff6404265049088
Daniel Dozie <31423288+badt0men@users.noreply.github.com>
2021-11-25 22:11:12 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   darkMode: "class",
   purge:{ 
-    enabled: true,
     content:["./pages/**/*.{js,ts,jsx,tsx}","./components/**/*.{js,ts,jsx,tsx}"],
     safelist: [
       "dark",
