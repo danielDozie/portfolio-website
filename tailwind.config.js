7b3e448a248ae6811c55559b8cf3a41839dab614
Daniel Dozie <31423288+badt0men@users.noreply.github.com>
2021-11-25 22:02:18 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     enabled: true,
     content:["./pages/**/*.{js,ts,jsx,tsx}","./components/**/*.{js,ts,jsx,tsx}"],
     safelist: [
+      "dark",
       "bg-purple-500",
       "bg-purple-300",
       "bg-pink-500",
