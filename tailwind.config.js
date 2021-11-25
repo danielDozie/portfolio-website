e8c6ccd173806373949dccc996ae68fdf265014b
Daniel Dozie <31423288+badt0men@users.noreply.github.com>
2021-11-25 21:54:27 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   darkMode: "class",
   purge:{ 
+    enabled: true,
     content:["./pages/**/*.{js,ts,jsx,tsx}","./components/**/*.{js,ts,jsx,tsx}"],
     safelist: [
       "bg-purple-500",
